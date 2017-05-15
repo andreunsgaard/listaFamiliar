d90cb1cb12565d2cd6d11bddaa83cd0a90ebd78b
Alberto Sierra <alberto.sierra@murciaeduca.es>
2017-05-15 10:09:59 +0200

    Lista Familiar controlando que el usuario solo pueda tener acceso a su lista

---
+++ common/models/lista-familiar.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function(Listafamiliar) {
+
+	Listafamiliar.beforeRemote("find", function(ctx, modelInstance, next) {
+		//...
+		var app = require('../../server/server.js')
+		var Usuario = app.models.Usuario;
+
+		console.log(ctx.req.accessToken);
+		Usuario.findById(ctx.req.accessToken.userId, function(err, usuario) {
+			if (err) next(err);
+
+			ctx.args.filter = {
+				"where": { // include usuarios for the owner
+					"id": usuario.listaFamiliarId
+				}
+			}
+			console.log(ctx.args);
+			next();
+
+		})
+	});
+
+};
---
+++ server/boot/automigrateDB.js
@@ -0,0 +1,52 @@
+module.exports = function(app) {
+  if (process.env.AUTOMIGRATE) {
+    var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+    app.dataSources.mysqlDs.automigrate(lbTables, function(er) {
+      if (er) throw er;
+      console.log('Loopback tables [', lbTables, '] created in ', app.dataSources.mysqlDs.adapter.name);
+      var listaFamiliarTables = ['Usuario', 'ListaFamiliar', 'Producto'];
+      app.dataSources.mysqlDs.automigrate(listaFamiliarTables, function(er) {
+        if (er) throw er;
+        console.log('Loopback tables [', listaFamiliarTables, '] created in ', app.dataSources.mysqlDs.adapter.name);
+
+        var Usuario = app.models.Usuario;
+        var Role = app.models.Role;
+        var RoleMapping = app.models.RoleMapping;
+
+
+        Usuario.count(function(err, count) {
+          if (err) throw err;
+
+          if (count === 0) {
+            Usuario.create([{
+              username: 'admin',
+              apellidos: 'Plataforma',
+              nombre: 'Administrador',
+              email: 'alberto.sierra@murciaeduca.es',
+              password: 'password'
+            }], function(err, users) {
+              if (err) throw err;
+
+              // Create the admin role
+              Role.create({
+                name: 'admin'
+              }, function(err, role) {
+                if (err) throw err;
+                console.log(role);
+
+                // Make Bob an admin
+                role.principals.create({
+                  principalType: RoleMapping.USER,
+                  principalId: users[0].id
+                }, function(err, principal) {
+                  if (err) throw err;
+                  console.log(principal);
+                });
+              });
+            });
+          }
+        });
+      });
+    });
+  }
+};
