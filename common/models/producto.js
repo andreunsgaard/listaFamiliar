efa9644612fc1bad3f8b6eebf925cc8f4048a928
Salva <salvador.e.medina@gmail.com>
2017-05-17 10:33:19 +0200

    añadir producto a la listafamiliar del usuario

--- common/models/producto.js
+++ common/models/producto.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = function(Producto) {
+	Producto.beforeRemote("create", function(ctx, modelInstance, next) {
+		var app = require('../../server/server.js')
+		var Usuario = app.models.Usuario;
+		Usuario.findById(ctx.req.accessToken.userId, function(err, usuario) {
+			if (err) next(err);
+			ctx.args.data.listaFamiliarId = usuario.listaFamiliarId;
+			next();
+		})
+	})
+	Producto.beforeRemote("update", function(ctx, modelInstance, next) {
+		var app = require('../../server/server.js')
+		var Usuario = app.models.Usuario;
+		Usuario.findById(ctx.req.accessToken.userId, function(err, usuario) {
+			if (err) next(err);
+			ctx.args.data.listaFamiliarId = usuario.listaFamiliarId;
+			next();
+		})
+	})
 
 };
