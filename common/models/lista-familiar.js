fcf55ab0c995bdfaf22b11e6638c8c794be9c023
angel <angelparmeno@gmail.com>
2017-05-22 08:02:09 +0200

    asociar tabla a autenticado

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -22,4 +22,21 @@ module.exports = function(Listafamiliar) {
 		})
 	});
 
+	//asociar al ususario autenticado la lista recien creada
+
+Listafamiliar.afterRemote("create", function(ctx, modelInstance, next) {
+		//...
+		var app = require('../../server/server.js')
+		var Usuario = app.models.Usuario;
+		Usuario.findById(ctx.req.accessToken.userId, function(err, usuario) {
+			if (err) next(err);
+			usuario.listaFamiliarId = modelInstance.id;
+			usuario.save(function(err, callback){
+				if (err) next(err);
+				next()
+			});
+		})
+	});
+
 };
+
